3f06196566eeff0e879dc89916be21f0f5bed9dd
kitaminka <kit4minka@gmail.com>
2025-04-15 21:44:46 +0300

    Add more packages, add sessions

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 import express from 'express';
-const app = express()
-const port = 3000
+import { Sequelize, DataTypes } from 'sequelize';
+import session from 'express-session';
+
+const app = express();
+const port = 3000;
+
+const sequelize = new Sequelize('test', 'kitam', '1234', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
+// const Wish = sequelize.define("Wish", {
+//     title: DataTypes.INTEGER,
+//     quantity: DataTypes.INTEGER,
+// });
+
+await sequelize.sync();
+
+app.use(session({
+    secret: 'keyboard cat',
+    saveUninitialized: true,
+    resave: true,
+}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    console.log(req.session)
+    if (!req.session.bebra) req.session.bebra = Math.random();
+    res.send(`${JSON.stringify(req.session)}`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
