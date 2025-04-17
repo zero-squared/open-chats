87e51ba32a2309285b3206d77cb12f81f6881488
kitaminka <kit4minka@gmail.com>
2025-04-17 16:52:04 +0300

    Add not found page, add body parser

--- app.js
+++ app.js
@@ -1,45 +1,16 @@
 import express from 'express';
-import { Sequelize, DataTypes } from 'sequelize';
+import { Sequelize } from 'sequelize';
 import session from 'express-session';
 import 'dotenv/config';
+import bodyParser from 'body-parser';
+
 import fs from 'fs';
 import https from 'https';
 import http from 'http';
 
 const app = express();
+
 const port = process.env.PORT || 3000;
-
-const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
-    logging: false,
-});
-try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-} catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
-
-// const Wish = sequelize.define("Wish", {
-//     title: DataTypes.INTEGER,
-//     quantity: DataTypes.INTEGER,
-// });
-
-//await sequelize.sync();
-
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: true,
-    resave: true,
-}));
-
-app.get('/', (req, res) => {
-    console.log(req.session)
-    if (!req.session.bebra) req.session.bebra = Math.random();
-    res.send(`${JSON.stringify(req.session)}`);
-});
-
 let server;
 
 if (process.env.NODE_ENV === 'production') {
@@ -53,6 +24,37 @@ if (process.env.NODE_ENV === 'production') {
     server = http.createServer(app);
 }
 
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, {
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    logging: false,
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Connected to the database');
+} catch (err) {
+    console.error('Unable to connect to the database:', err);
+}
+
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    if (!req.session.bebra) req.session.bebra = Math.random();
+    res.send(`${JSON.stringify(req.session)}`);
+});
+app.use((req, res) => {
+    return res.status(404).send('Not Found');
+});
+
 server.listen(port, () => {
-    console.log(`Web server started on ${port}`);
+    console.log(`Server started on port ${port}`);
 });
